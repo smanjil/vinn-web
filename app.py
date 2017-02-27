d6dc6b9bbd895c6625e18a26cccb4d88fb501a10
Manjil Shrestha <manjil.shrestha@yipl.com.np>
2017-02-27 10:19:52 +0545

    [master]organization/user login, pulled respective org_id from users table after credentials match

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
-import json
-from models import IncomingLog
+from flask import Flask, render_template, request, redirect
+from models import IncomingLog, Users
 app = Flask(__name__)
 
 
-@app.route('/reportraw')
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           title='Home')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    print username, password
+    try:
+        user = Users.select().where(Users.username == username, Users.password == password)[0]
+    except:
+        raise RuntimeError('User does not exist!')
+    else:
+        print user.org_id
+    return redirect('/nuwakotreport')
+
+
+@app.route('/nuwakotreport')
 def testreport():
     reports = []
-    for i in IncomingLog.select().where(IncomingLog.extension==3001):
+    for i in IncomingLog.select().where(IncomingLog.extension == 3001):
         item = []
         item.append(i.incoming_number)
         item.append(i.call_start_time)
@@ -18,7 +37,7 @@ def testreport():
         org_name = i.org.name
         to = i.extension
         reports.append(item)
-    print '\n\n', reports, '\n\n'
+#    print '\n\n', reports, '\n\n'
     return render_template(
         'test_report.html',
         title='Report',
@@ -26,31 +45,6 @@ def testreport():
         org_name=org_name,
         to=to
     )
-
-
-@app.route('/')
-def index():
-    return render_template(
-        'report.html',
-        title='Report'
-    )
-
-
-@app.route('/report', methods=['GET'])
-def get_report():
-    output = {
-        'vboard': {
-            'vboard1': False,
-            'vboard2': True
-        },
-        'vsurvey': {
-            'status': True,
-            'question1': 'file1',
-            'dtmf': '1',
-            'question2': 'file2'
-        }
-    }
-    return json.dumps(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
