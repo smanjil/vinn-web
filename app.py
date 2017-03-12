f76f26f05dca0339bbbd490eede342c7a4dd6c19
Manjil Shrestha <shresthamanjil21@gmail.com>
2017-03-12 11:59:01 +0545

    [vue-prototype] migrate

--- app.py
+++ app.py
@@ -1,110 +1,111 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy.dialects.postgresql import JSON
 
-# -*- coding: utf-8 -*-
-
-from flask import Flask, render_template, request, redirect, url_for, session, flash
-from models import IncomingLog, Users, Services, Comment
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/voiceinndbmodels'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # set the secret key.  keep this really secret: required to use session
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
-
-@app.route('/')
-def index():
-    return render_template('index.html',
-                           title='Home')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-    try:
-        user = Users.select().where(Users.username == username,
-                                    Users.password == password)[0]
-    except:
-        flash('Username/Password does not match')
-        return redirect(url_for('index'))
-    else:
-        session['logged_in'] = True
-        session['org_id'] = user.org_id
-        session['username'] = username
-    return redirect(url_for('get_report', org_id=user.org_id))
+class Organization(db.Model):
+    __tablename__ = 'Organization'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    created = db.Column(db.DateTime)
+    location = db.Column(db.String(100))
+    phone = db.Column(db.Integer)
+    email = db.Column(db.String)
 
 
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    return redirect(url_for('index'))
+class Service(db.Model):
+    __tablename__ = 'Service'
+    id = db.Column(db.Integer, primary_key = True)
+    org_id = db.Column(db.Integer, db.ForeignKey('organization.id'))    
+    service_type = db.Column(db.Integer)
+    service_id = db.Column(db.Integer)
+    extension = db.Column(db.Integer)
+    isActive = db.Column(db.Boolean)
+    created = db.Column(db.DateTime)
+    allocated_channels = db.Column(db.Integer)
+    channels_in_use = db.Column(db.Integer)
 
 
-@app.route('/pullreport', methods=['GET', 'POST'])
-def get_report():
-    org_id = request.args['org_id']
-    session_org_id = session.get('org_id', None)
-
-    if session.get('logged_in') is not None and str(session_org_id) == org_id:
-        print 'Organization id: ', org_id
-        service = Services.select().where(Services.org_id == org_id)
-        print 'Services rows: ', len(service)
-        services = [[items.service_type.name, items.extension] for items in service]
-
-        i = IncomingLog.select().where(IncomingLog.org == org_id)
-        org_name = i[0].org.name
-
-        if request.method == 'POST':
-            print request.form
-            if 'commentform' in request.form:
-                log_id = request.form['log_id']
-                comment = request.form['comment']
-                Comment.create(log_id=log_id, comment=comment)
-            if 'status' in request.form:
-                log_id = request.form['log_id']
-                status = request.form['status']
-                print status
-                i = IncomingLog.update(status=status).where(IncomingLog.id == log_id)
-                i.execute()
-
-        if 'service' in request.args and 'extension' in request.args:
-            service_name = str(request.args['service'])
-            extension = str(request.args['extension'])
-
-            reports = []
-            try:
-                for i in IncomingLog.select().where(IncomingLog.org == org_id, IncomingLog.service == service_name, IncomingLog.extension == extension).order_by(IncomingLog.id.desc()):
-                    item = []
-                    item.append(i.incoming_number)
-                    item.append(i.call_start_time)
-                    item.append(i.generalized_data_incoming.data)
-                    item.append(i.id)
-                    item.append(i.comment_set)
-                    item.append(i.status)
-                    org_name = i.org.name
-                    reports.append(item)
-                return render_template(
-                    'report.html',
-                    title='Report',
-                    output=reports,
-                    org_name=org_name,
-                    to=extension,
-                    org_id=org_id,
-                    services=services,
-                    service_name=service_name
-                )
-            except:
-                flash('No reports available for this service!')
-                return redirect(url_for('get_report', org_id=org_id))
-        else:
-            return render_template(
-                'report.html',
-                title='Report',
-                org_id=org_id,
-                services=services,
-                org_name=org_name
-            )
-    else:
-        return redirect(url_for('index'))
+class User(db.Model):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key = True)
+    org_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
+    name = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    phone = db.Column(db.Integer)
+    email = db.Column(db.String(100))
+    created = db.Column(db.DateTime)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+class ServiceType(db.Model):
+    __tablename__ = 'ServicecTypei'
+    id = db.Column(db.Integer, primary_key = True)
+    s_type_id = db.Column(db.Integer, db.ForeignKey('service.service_type'))
+    name = db.Column(db.String(100))
+
+
+class GeneralizedDialplan(db.Model):
+    __tablename__ = 'GeneralizedDialplan'
+    id = db.Column(db.Integer, primary_key = True)
+    s_id = db.Column(db.Integer, db.ForeignKey('service.service_id'))
+    dialplan = db.Column(JSON)
+    previous_version_id = db.Column(db.Integer)
+
+
+class GeneralizedDataIncoming(db.Model):
+    __tablename__ = 'GeneralizedDataIncoming'
+    id = db.Column(db.Integer, primary_key = True)
+    g_id = db.Column(db.Integer, db.ForeignKey('generalizeddialplan.id'))
+    generalized_dialplan_id = db.Column(db.Integer)
+    data = db.Column(JSON)
+    incoming_number = db.Column(db.Integer)
+
+
+class GeneralizedDataOutgoing(db.Model):
+    __tablename__ = 'GeneralizedDataOutgoing'
+    id = db.Column(db.Integer, primary_key = True)
+    g_id = db.Column(db.Integer, db.ForeignKey('generalizeddialplan.id'))
+    generalized_dialplan_id = db.Column(db.Integer)
+    data = db.Column(JSON)
+    outgoing_number = db.Column(db.Integer)
+
+
+class IncomingLog(db.Model):
+    __tablename__ = 'IncomingLog'
+    id = db.Column(db.Integer, primary_key = True)
+    org_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
+    service = db.Column(db.String(100))
+    call_time = db.Column(db.DateTime)
+    call_duration = db.Column(db.Float)
+    complete = db.Column(db.Boolean)
+    phone = db.Column(db.Integer)
+    status = db.Column(db.String(100))
+
+
+class OutgoingLog(db.Model):
+    __tablename__ = 'OutgoingLog'
+    id = db.Column(db.Integer, primary_key = True)
+    org_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
+    service = db.Column(db.String(100))
+    call_time = db.Column(db.DateTime)
+    call_duration = db.Column(db.Float)
+    complete = db.Column(db.Boolean)
+    phone = db.Column(db.Integer)
+    status = db.Column(db.String(100))
+
+
+class Comment(db.Model):
+    __tablename__ = 'Comment'
+    id = db.Column(db.Integer, primary_key = True)
+    il_id = db.Column(db.Integer, db.ForeignKey('incominglog.id'))
+    comment = db.Column(db.String(500))
---
+++ views.py
@@ -0,0 +1,110 @@
+
+# -*- coding: utf-8 -*-
+
+from flask import render_template, request, redirect, url_for, session, flash
+from models import IncomingLog, Users, Services, Comment
+
+#app = Flask(__name__)
+
+# set the secret key.  keep this really secret: required to use session
+#app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           title='Home')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    try:
+        user = Users.select().where(Users.username == username,
+                                    Users.password == password)[0]
+    except:
+        flash('Username/Password does not match')
+        return redirect(url_for('index'))
+    else:
+        session['logged_in'] = True
+        session['org_id'] = user.org_id
+        session['username'] = username
+    return redirect(url_for('get_report', org_id=user.org_id))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/pullreport', methods=['GET', 'POST'])
+def get_report():
+    org_id = request.args['org_id']
+    session_org_id = session.get('org_id', None)
+
+    if session.get('logged_in') is not None and str(session_org_id) == org_id:
+        print 'Organization id: ', org_id
+        service = Services.select().where(Services.org_id == org_id)
+        print 'Services rows: ', len(service)
+        services = [[items.service_type.name, items.extension] for items in service]
+
+        i = IncomingLog.select().where(IncomingLog.org == org_id)
+        org_name = i[0].org.name
+
+        if request.method == 'POST':
+            print request.form
+            if 'commentform' in request.form:
+                log_id = request.form['log_id']
+                comment = request.form['comment']
+                Comment.create(log_id=log_id, comment=comment)
+            if 'status' in request.form:
+                log_id = request.form['log_id']
+                status = request.form['status']
+                print status
+                i = IncomingLog.update(status=status).where(IncomingLog.id == log_id)
+                i.execute()
+
+        if 'service' in request.args and 'extension' in request.args:
+            service_name = str(request.args['service'])
+            extension = str(request.args['extension'])
+
+            reports = []
+            try:
+                for i in IncomingLog.select().where(IncomingLog.org == org_id, IncomingLog.service == service_name, IncomingLog.extension == extension).order_by(IncomingLog.id.desc()):
+                    item = []
+                    item.append(i.incoming_number)
+                    item.append(i.call_start_time)
+                    item.append(i.generalized_data_incoming.data)
+                    item.append(i.id)
+                    item.append(i.comment_set)
+                    item.append(i.status)
+                    org_name = i.org.name
+                    reports.append(item)
+                return render_template(
+                    'report.html',
+                    title='Report',
+                    output=reports,
+                    org_name=org_name,
+                    to=extension,
+                    org_id=org_id,
+                    services=services,
+                    service_name=service_name
+                )
+            except:
+                flash('No reports available for this service!')
+                return redirect(url_for('get_report', org_id=org_id))
+        else:
+            return render_template(
+                'report.html',
+                title='Report',
+                org_id=org_id,
+                services=services,
+                org_name=org_name
+            )
+    else:
+        return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
